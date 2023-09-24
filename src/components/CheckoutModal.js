fdce188729d3c9bf36011838776357e0ba2ef619
swoops503 <james.roundy4@gmail.com>
2023-09-23 21:02:29 -0700

    added back button to checkoutmodal

--- src/components/CheckoutModal.js
+++ src/components/CheckoutModal.js
@@ -12,6 +12,10 @@ const CheckoutModal = ({ isOpen, onClose, onCheckoutSuccess }) => {
     fullName: '', // New field for full name
     city: '', // New field for city
     state: '', // New field for state
+    zipCode: '', // New field for zip code
+    creditCard: '', // New field for credit card
+    cvv: '', // New field for CVV
+    billingZipCode: '', // New field for billing zip code
   });
   const [isCheckoutSuccessful, setCheckoutSuccessful] = useState(false);
   const [error, setError] = useState('');
@@ -22,12 +26,23 @@ const CheckoutModal = ({ isOpen, onClose, onCheckoutSuccess }) => {
   };
 
   const handleCheckout = () => {
-    if (!shippingInfo.shippingAddress || !shippingInfo.billingAddress || !shippingInfo.fullName || !shippingInfo.city || !shippingInfo.state) {
+    if (
+      !shippingInfo.shippingAddress ||
+      !shippingInfo.billingAddress ||
+      !shippingInfo.fullName ||
+      !shippingInfo.city ||
+      !shippingInfo.state ||
+      !shippingInfo.zipCode ||
+      !shippingInfo.creditCard ||
+      !shippingInfo.cvv ||
+      !shippingInfo.billingZipCode
+    ) {
       setError('Please fill in all required fields.');
     } else {
       setError('');
       setTimeout(() => {
         setCheckoutSuccessful(true);
+        onCheckoutSuccess();
         clearCart();
       }, 2000);
     }
@@ -43,7 +58,10 @@ const CheckoutModal = ({ isOpen, onClose, onCheckoutSuccess }) => {
     <>
       {/* Checkout Modal */}
       <div className={`fixed top-0 left-0 w-full h-full flex items-center justify-center ${isOpen ? '' : 'hidden'}`}>
-        <div className="bg-white w-[80%] md:w-[50%] lg:w-[40%] xl:w-[30%] p-4 shadow-2xl rounded-lg">
+        <div className="bg-white w-[80%] md:w-[50%] lg:w-[40%] xl:w-[30%] p-4 shadow-2xl rounded-lg relative">
+          <div className="absolute top-2 right-2 cursor-pointer" onClick={onClose}>
+            <IoMdArrowForward className="text-2xl" />
+          </div>
           <div className="text-center uppercase text-sm font-semibold">
             Shopping Bag ({itemAmount})
           </div>
@@ -56,7 +74,7 @@ const CheckoutModal = ({ isOpen, onClose, onCheckoutSuccess }) => {
               name="fullName"
               value={shippingInfo.fullName}
               onChange={handleInputChange}
-              placeholder="Full Name"
+              placeholder="Full Name*"
               required
             />
             <input
@@ -64,7 +82,7 @@ const CheckoutModal = ({ isOpen, onClose, onCheckoutSuccess }) => {
               name="shippingAddress"
               value={shippingInfo.shippingAddress}
               onChange={handleInputChange}
-              placeholder="Shipping Address"
+              placeholder="Shipping Address*"
               required
             />
             <input
@@ -72,7 +90,7 @@ const CheckoutModal = ({ isOpen, onClose, onCheckoutSuccess }) => {
               name="billingAddress"
               value={shippingInfo.billingAddress}
               onChange={handleInputChange}
-              placeholder="Billing Address"
+              placeholder="Billing Address*"
               required
             />
             <input
@@ -80,7 +98,7 @@ const CheckoutModal = ({ isOpen, onClose, onCheckoutSuccess }) => {
               name="city"
               value={shippingInfo.city}
               onChange={handleInputChange}
-              placeholder="City"
+              placeholder="City*"
               required
             />
             <input
@@ -88,10 +106,44 @@ const CheckoutModal = ({ isOpen, onClose, onCheckoutSuccess }) => {
               name="state"
               value={shippingInfo.state}
               onChange={handleInputChange}
-              placeholder="State"
+              placeholder="State*"
+              required
+            />
+            <input
+              type="text"
+              name="zipCode"
+              value={shippingInfo.zipCode}
+              onChange={handleInputChange}
+              placeholder="Zip Code*"
+              required
+            />
+            <hr className="my-4 border-t border-gray-300" />
+            <input
+              type="text"
+              name="creditCard"
+              value={shippingInfo.creditCard}
+              onChange={handleInputChange}
+              placeholder="Credit Card (16 digits)*"
+              required
+            />
+            <input
+              type="text"
+              name="cvv"
+              value={shippingInfo.cvv}
+              onChange={handleInputChange}
+              placeholder="CVV (3 digits)*"
+              required
+            />
+            <input
+              type="text"
+              name="billingZipCode"
+              value={shippingInfo.billingZipCode}
+              onChange={handleInputChange}
+              placeholder="Billing Zip Code (6 digits)*"
               required
             />
           </div>
+          <hr className="my-4 border-t border-gray-300" />
           <div className="font-semibold">
             <span>Total:</span> $ {parseFloat(total).toFixed(2)}
           </div>
@@ -102,9 +154,6 @@ const CheckoutModal = ({ isOpen, onClose, onCheckoutSuccess }) => {
           <button onClick={handleCheckout} className="bg-primary flex p-4 justify-center items-center text-white w-full font-medium">
             Checkout
           </button>
-          <div onClick={onClose} className="absolute top-2 right-2 cursor-pointer">
-            <IoMdArrowForward className="text-2xl" />
-          </div>
         </div>
       </div>
 
